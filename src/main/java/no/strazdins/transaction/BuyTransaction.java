3242fd6ca365cccece0ee9c2d8f2d127d94f80d2
Girts Strazdins <girts.strazdins@gmail.com>
2023-04-05 10:14:27 +0200

    First BNB buy-transaction implemented

--- src/main/java/no/strazdins/transaction/BuyTransaction.java
+++ src/main/java/no/strazdins/transaction/BuyTransaction.java
@@ -1,5 +1,6 @@
 package no.strazdins.transaction;
 
+import no.strazdins.data.Decimal;
 import no.strazdins.data.ExtraInfoEntry;
 import no.strazdins.data.Operation;
 import no.strazdins.data.RawAccountChange;
@@ -41,8 +42,41 @@ public class BuyTransaction extends Transaction {
 
   @Override
   public WalletSnapshot process(WalletSnapshot walletSnapshot, ExtraInfoEntry extraInfo) {
-    // TODO
-    // !!! throw new UnsupportedOperationException();
-    return walletSnapshot;
+    WalletSnapshot newSnapshot = walletSnapshot.prepareForTransaction(this);
+    fee = feeOp.getAmount();
+    feeCurrency = feeOp.getAsset();
+    quoteCurrency = quote.getAsset();
+    quoteAmount = quote.getAmount();
+    baseCurrency = base.getAsset();
+    baseCurrencyAmount = base.getAmount();
+
+    calculateFeeInUsdt(newSnapshot.getWallet());
+
+
+    if (feeInUsdt.isZero() && feeOp.getAsset().equals("BNB") && base.getAsset().equals("BNB")) {
+      return processFirstBnbBuy(newSnapshot);
+//    } else if (quote.getAsset().equals("BNB")) {
+//      return processBuyWithBnbFee();
+//    } else if (quote.getAsset().equals("USDT")) {
+//      return processBuyWithUsdtFee();
+    } else {
+      throw new UnsupportedOperationException("Unknown type of buy transaction: " + this);
+    }
+  }
+
+  private WalletSnapshot processFirstBnbBuy(WalletSnapshot newSnapshot) {
+    Decimal usdtUsedInTransaction = quote.getAmount().negate();
+    newSnapshot.decreaseAsset(quote.getAsset(), usdtUsedInTransaction);
+
+    Decimal obtainedBnb = base.getAmount().subtract(fee.negate());
+    Decimal avgBnbPrice = usdtUsedInTransaction.divide(obtainedBnb).multiply(
+        newSnapshot.getWallet().getAvgObtainPrice(quote.getAsset())
+    );
+    newSnapshot.addAsset("BNB", obtainedBnb, avgBnbPrice);
+
+    baseObtainPriceInUsdt = avgBnbPrice;
+    feeInUsdt = fee.multiply(avgBnbPrice).negate();
+
+    return newSnapshot;
   }
 }
--- src/test/java/ScenarioTest.java
+++ src/test/java/ScenarioTest.java
@@ -1,7 +1,5 @@
 import no.strazdins.data.*;
-import no.strazdins.transaction.DepositTransaction;
-import no.strazdins.transaction.SellTransaction;
-import no.strazdins.transaction.Transaction;
+import no.strazdins.transaction.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,18 +13,24 @@ class ScenarioTest {
   void testScenario() {
     WalletSnapshot ws1 = WalletSnapshot.createEmpty();
     WalletSnapshot ws2 = processDeposit(ws1, "LTC", "1.65167383", "72.22");
-    expectWalletState(ws2, 1, "1.65167383", "72.22", "0");
-    expectPnl(ws2, "0", "0");
+    expectWalletState(ws2, 1, "1.65167383", "72.22",
+        "0", "0", "0");
 
     WalletSnapshot ws3 = processSell(ws2, "LTC", "0.65167000",
         "47.115741", "0.04711574", "USDT");
-    expectWalletState(ws3, 2, "1.00000383", "72.22", "47.06862526");
-    expectPnl(ws3, "0.00501786", "0.00501786");
+    expectWalletState(ws3, 2, "1.00000383", "72.22",
+        "47.06862526","0.00501786", "0.00501786");
 
     WalletSnapshot ws4 = processSell(ws3, "LTC", "1.00000000",
         "72.30000000", "0.07230000", "USDT");
-    expectWalletState(ws4, 2, "0.00000383", "72.22", "119.29632526");
-    expectPnl(ws4, "0.00770000", "0.01271786");
+    expectWalletState(ws4, 2, "0.00000383", "72.22",
+        "119.29632526", "0.00770000", "0.01271786");
+
+    WalletSnapshot ws5 = processBuy(ws4, "BNB", "1.00000000",
+        "20.28760000", "USDT", "0.00075000", "BNB");
+    expectWalletState(ws5, 3, "0.00000383", "72.22",
+        "99.00872526", "0", "0.01271786");
+    expectAssetAmount(ws5, "BNB", "0.99925", "20.30282712");
   }
 
   private WalletSnapshot processDeposit(WalletSnapshot startSnapshot,
@@ -38,6 +42,23 @@ class ScenarioTest {
     ExtraInfoEntry ei = new ExtraInfoEntry(transactionTime, ExtraInfoType.ASSET_PRICE, obtainPrice);
     DepositTransaction deposit = new DepositTransaction(t);
     return deposit.process(startSnapshot, ei);
+  }
+
+  private WalletSnapshot processBuy(WalletSnapshot startSnapshot, String asset, String amount,
+                                    String usedQuote, String quoteCurrency, String fee,
+                                    String feeCurrency) {
+    transactionTime += 1000;
+    Transaction t = new Transaction(transactionTime);
+    t.append(new RawAccountChange(transactionTime, AccountType.SPOT,
+        Operation.BUY, asset, new Decimal(amount), "Buy coin"));
+    t.append(new RawAccountChange(transactionTime, AccountType.SPOT,
+        Operation.TRANSACTION_RELATED, quoteCurrency, new Decimal(usedQuote).negate(),
+        "Sell " + quoteCurrency));
+    t.append(new RawAccountChange(transactionTime, AccountType.SPOT,
+        Operation.FEE, feeCurrency, new Decimal(fee).negate(), "Fee in " + feeCurrency));
+    BuyTransaction buy = new BuyTransaction(t);
+    return buy.process(startSnapshot, null);
+
   }
 
   private WalletSnapshot processSell(WalletSnapshot startSnapshot, String asset, String amount,
@@ -55,10 +76,12 @@ class ScenarioTest {
   }
 
   private void expectWalletState(WalletSnapshot ws, int assetCount,
-                                 String ltcAmount, String ltcObtainPrice, String usdtAmount) {
+                                 String ltcAmount, String ltcObtainPrice, String usdtAmount,
+                                 String transactionPnl, String runningPnl) {
     assertEquals(assetCount, ws.getWallet().getAssetCount());
     expectAssetAmount(ws, "LTC", ltcAmount, ltcObtainPrice);
     expectAssetAmount(ws, "USDT", usdtAmount, "1.0");
+    expectPnl(ws, transactionPnl, runningPnl);
   }
 
   private void expectAssetAmount(WalletSnapshot ws, String asset, String amount,
