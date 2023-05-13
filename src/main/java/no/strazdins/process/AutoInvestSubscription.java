a3db3f1f750d666fc33b2b1d0bdb6b1a35f23a87
Girts Strazdins <girts.strazdins@gmail.com>
2023-05-13 22:32:33 +0200

    AutoInvestSubscription now supports asset proportions and validity check

--- src/main/java/no/strazdins/process/AutoInvestSubscription.java
+++ src/main/java/no/strazdins/process/AutoInvestSubscription.java
@@ -1,5 +1,7 @@
 package no.strazdins.process;
 
+import java.util.HashMap;
+import java.util.Map;
 import no.strazdins.data.Decimal;
 
 /**
@@ -8,13 +10,52 @@ import no.strazdins.data.Decimal;
  */
 public class AutoInvestSubscription {
   private final Decimal investmentAmount;
+  private final Map<String, Decimal> assetProportions = new HashMap<>();
 
   /**
    * Create auto-invest subscription.
    *
    * @param investmentAmount The investment amount, in USD
+   * @throws IllegalArgumentException When investment amount is null or non-positive
    */
-  public AutoInvestSubscription(Decimal investmentAmount) {
+  public AutoInvestSubscription(Decimal investmentAmount) throws IllegalArgumentException {
+    if (investmentAmount == null || !investmentAmount.isPositive()) {
+      throw new IllegalArgumentException("Investment amount must be positive, "
+          + investmentAmount + " provided");
+    }
     this.investmentAmount = investmentAmount;
+  }
+
+  /**
+   * Register what proportion of the investment will be used to acquire the given asset.
+   *
+   * @param asset      The asset in question
+   * @param proportion The proportion of invested money used to buy this asset in each buy
+   * @throws IllegalArgumentException When trying to register proportion for an asset which has
+   *                                  already a previous proportion registered
+   */
+  public void addAssetProportion(String asset, Decimal proportion) throws IllegalArgumentException {
+    if (assetProportions.containsKey(asset)) {
+      throw new IllegalArgumentException("Proportion for " + asset + " already registered");
+    }
+    assetProportions.put(asset, proportion);
+  }
+
+  /**
+   * Check whether the subscription is valid - whether it has all necessary information.
+   *
+   * @return True if the subscription is valid (information is complete), false otherwise
+   */
+  public boolean isValid() {
+    return investmentAmount != null && investmentAmount.isPositive()
+        && allProportionsSumUpToOne();
+  }
+
+  private boolean allProportionsSumUpToOne() {
+    Decimal proportionSum = Decimal.ZERO;
+    for (Decimal proportion : assetProportions.values()) {
+      proportionSum = proportionSum.add(proportion);
+    }
+    return proportionSum.equals(Decimal.ONE);
   }
 }
